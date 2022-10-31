a70351ecedf348c5616e4903a014ad0579c3ce87
LoganKells <lkells@outlook.com>
2022-10-31 11:57:32 -0400

    Created MapsActivity.kt

---
+++ app/src/main/java/com/example/cityexplorer/MapsActivity.kt
@@ -0,0 +1,40 @@
+package com.example.cityexplorer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cityexplorer.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var activityMapsBinding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Retrieve the content view that renders the map.
+        activityMapsBinding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(activityMapsBinding.root)
+
+        // Get the SupportMapFragment and request notification when the map is ready to be used.
+        val mapFragment: SupportMapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        // TODO the map marker should be based on data passed to this activity about the
+        //  Location that was selected by the user.
+        val sydney = LatLng(-33.852, 151.211)
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney")
+        )
+    }
+}
